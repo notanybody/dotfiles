ea5f1a795c6f0faa25592d12aa0dfdfcfd3bc67c
notanybody <jackwillisnyc@gmail.com>
2026-05-19 17:17:42 -0400

    update dotfiles

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,8 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Strip Windows line endings (^M) on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  command = "%s/\\r//ge",
+})
